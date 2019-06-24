30dc9bc2b91aba2d4b181c171206e7befb8bf6a5
Yazid <yazasykurillah@gmail.com>
2019-06-24 13:50:25 +0700

    Revert accounted settlement in payroll page

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -238,6 +238,7 @@ class PayrollController extends Controller
         //get user's settlements
         $settlements = Settlement::with('internal_request')
             ->where('status','=','approved')
+            ->where('accounted',FALSE)
             ->whereHas('internal_request', function($query) use($user, $period){
                 $query->where('requester_id', '=', $user->id);
                 $query->whereBetween('transaction_date', [$period->start_date, $period->end_date]);
@@ -608,6 +609,7 @@ class PayrollController extends Controller
         $settlement_balance = 0;
         $settlements = Settlement::with('internal_request')
             ->where('status','=','approved')
+            ->where('accounted',FALSE)
             ->whereHas('internal_request', function($query) use($user, $period){
                 $query->where('requester_id', '=', $user->id);
                 $query->whereBetween('transaction_date', [$period->start_date, $period->end_date]);
