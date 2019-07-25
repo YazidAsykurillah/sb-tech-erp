6f91417d8c933788fb03a57f9962b042b0c4b5bf
Yazid <yazasykurillah@gmail.com>
2019-07-25 12:34:01 +0700

    Fix payroll

--- app/Listeners/RegisterSettlementPayroll.php
+++ app/Listeners/RegisterSettlementPayroll.php
@@ -34,11 +34,12 @@ class RegisterSettlementPayroll
         $period = $payroll->period;
         $user = $payroll->user;
         //get user's settlements
-        $end_period_date = Carbon::parse($period->end_date)->addDay(3);
+        $end_period_date = Carbon::parse($period->end_date)->addDay(4);
         $settlements = Settlement::with('internal_request')
             ->where('status','=','approved')
             ->where('accounted', FALSE)
-            ->whereBetween('transaction_date', [$period->start_date, $end_period_date->format('Y-m-d')])
+            ->whereBetween('updated_at', [$period->start_date->format('Y-m-d H:i:s'), $end_period_date->format('Y-m-d H:i:s')])
+            ->orWhereBetween('transaction_date', [$period->start_date, $end_period_date->format('Y-m-d')])
             ->whereHas('internal_request', function($query) use($user, $period){
                 $query->where('requester_id', '=', $user->id);
                 //$query->whereBetween('transaction_date', [$period->start_date, $period->end_date]);
