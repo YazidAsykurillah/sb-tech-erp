d31a6812da63173c3bd85a08decdff60c8cfed4c
Yazid <yazasykurillah@gmail.com>
2019-08-09 15:34:16 +0700

    Add custom validation rules max amount on store invoice vendor

--- app/Http/Requests/StoreInvoiceVendorRequest.php
+++ app/Http/Requests/StoreInvoiceVendorRequest.php
@@ -3,9 +3,30 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
-
+use Illuminate\Validation\Factory;
+use App\PurchaseOrderVendor;
 class StoreInvoiceVendorRequest extends Request
 {
+    public function __construct(Factory $factory)
+    {
+
+        $name = 'is_max_amount'; // a name for our custom rule, to be referenced in `rules` below
+        
+        $test = function ($_x, $value, $_y) {
+            $purchase_order = PurchaseOrderVendor::findOrFail(\Request::get('purchase_order_vendor_id'));
+
+            //$max = floatval(\Request::get('total_invoice_due'));
+           
+            $cleared_max = $purchase_order->amount+0.1;
+            $cleared_value = floatval(preg_replace('#[^0-9.]#', '', $value));
+            //echo $cleared_value;exit();
+            return $cleared_value < $cleared_max;
+        };
+        $errorMessage = 'Can not more than purchase order amount';
+
+        $factory->extend($name, $test, $errorMessage);
+    }
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -30,7 +51,7 @@ class StoreInvoiceVendorRequest extends Request
             'project_id'=>'required|integer',
             'purchase_order_vendor_id'=>'required|integer',
             'type'=>'required',
-            'amount'=>'required',
+            'amount'=>'required|is_max_amount',
             'due_date'=>'required',
             'received_date'=>'required',
             'tax_date'=>'required_with:tax_number'
