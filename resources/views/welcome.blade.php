4d3d12f82937b58416fbd8a7d2d83b6332cb56fb
YazidAsykurillah <asykurillahy@gmail.com>
2019-09-24 14:19:40 +0700

    Fix welcome to use app name config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
             
                 <div class="intro-message" style="color:#fff;">
                     <div class="col-md-8">
-                        <h1>BMKN ACCOUNTING</h1>
+                        <h1>{{config('app.name')}}</h1>
                         <hr>
 
                     </div>
