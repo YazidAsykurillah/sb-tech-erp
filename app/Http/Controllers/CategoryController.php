4678b13bfbe984fea5360f0d471b9454fdd256ac
YazidAsykurillah <yazasykurillah@gmail.com>
2019-10-01 07:56:37 +0700

    Fix save category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -39,7 +39,12 @@ class CategoryController extends Controller
      */
     public function store(StoreCategoryRequest $request)
     {
-        //
+        $category = new Category;
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->save();
+        return redirect('master-data/category/'.$category->id)
+            ->with('successMessage', "Category has been added");
     }
 
     /**
